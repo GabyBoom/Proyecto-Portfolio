765f6a80b5244ac1cb82e21723272fae855b4362
Gabriel <gaby_lamuerte@hotmail.com>
2023-08-29 14:16:49 -0300

    agregué eñ toggle style switcher

---
+++ js/style-switcher.js
@@ -0,0 +1,5 @@
+/*________________Toggle Style Switcher____________________*/
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () =>{
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
